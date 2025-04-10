01fc399c08dd51f2f746ffbbe51e95a666c2d193
YoDa <netski@internet.ru>
2025-04-10 12:27:19 +0300

    Amend commit

--- src/test/kotlin/WallServiceTests.kt
+++ src/test/kotlin/WallServiceTests.kt
@@ -1,6 +1,7 @@
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertFalse
 
 class WallServiceTest {
     @Before
@@ -20,7 +21,7 @@ class WallServiceTest {
     }
 
     @Test
-    fun updateExisting() {
+    fun updateExistingTrue() {
         // создаём целевой сервис
         val service = WallService
         // заполняем несколькими постами
@@ -32,5 +33,19 @@ class WallServiceTest {
         // выполняем целевое действие
         val result = service.update(update)
         assertTrue(result) // true, если пост обновлен
+    }
+    @Test
+    fun updateExistingFalse() {
+        // создаём целевой сервис
+        val service = WallService
+        // заполняем несколькими постами
+        service.add(Post(1, 1, 1, 1, "Поздравляем Вас с днем рождения", 1, true, true, true, true, Reposts(10, true, 1), Answer(1, 1)))
+        service.add(Post(2, 1, 1, 2, "Новый год", 1, true, true, true, true, Reposts(10, true, 2), Answer(1, 1)))
+        // создаём информацию об обновлении
+        val update = Post(3, 1, 1, 2, "Новый год", 5, true, true, true, true, Reposts(10, true, 2), Answer(3, 1))
+
+        // выполняем целевое действие
+        val result = service.update(update)
+        assertFalse(result) // false, если пост не обновлен
     }
 }
