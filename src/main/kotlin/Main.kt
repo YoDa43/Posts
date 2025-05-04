2388ab1b881796fc42eba5ce77129933f3bc6567
YoDa <netski@internet.ru>
2025-05-04 19:52:42 +0300

    Pull request Posts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,15 +7,15 @@ data class Post(
     val fromId: Int,  //Идентификатор автора записи (от чьего имени опубликована запись).
     val date: Int,  //Время публикации записи в формате unixtime.
     val text: String,  //Текст записи.
-    val likes: Int,  //Количество лайков.
+    var likes: Int?,  //Количество лайков.
     val canPin: Boolean,  //Информация о том, может ли текущий пользователь закрепить запись. Возможные значения: 1 — может, 0 — не может.
     val canDelete: Boolean,  //Информация о том, может ли текущий пользователь удалить запись. Возможные значения: 1 — может, 0 — не может.
     val canEdit: Boolean,  //Информация о том, может ли текущий пользователь редактировать запись. Возможные значения: 1 — может, 0 — не может.
     val isPinned: Boolean,  //Информация о том, прикреплена ли запись. Возможные значения: 1 — запись прикреплена, 0 — запись не прикреплена.
     val reposts: Reposts,
-    val answer: Answer
+    val answer: Answer,
+    val attachments: List<Any> = listOf<Attachment>()
 )
-
 object WallService {
     var posts = emptyArray<Post>()
     fun clear() {
@@ -24,10 +24,12 @@ object WallService {
     }
     private var count = 0
     fun add(post: Post): Post {
+        if (post.likes == null) {
+            post.likes = 0
+        }
         posts += post.copy(id = ++count)
         return posts.last()
     }
-
     fun update(post: Post): Boolean {
         for ((index, p) in posts.withIndex()) {
             if (p.id == post.id) {
@@ -45,11 +47,16 @@ object WallService {
     }
 }
 
+data class Attachment(val type: String)
+data class Photo(val id: Int,val ownerId: Int,val photo130: String,val photo604: String)
+data class PhotoAttachments(val photo: Photo)
+data class Video(val id: Int,val ownerId: Int,val title: String,val duration: Int)
+data class VideoAttachments(val video: Video)
+
 fun main() {
-    WallService.add(Post(1, 1, 1, 1, "Поздравляем Вас с днем рождения", 1, true, true, true, true, Reposts(10, true, 1), Answer(1, 1)))
-    WallService.add(Post(1, 1, 1, 2, "Новый год", 1, true, true, true, true, Reposts(10, true, 2), Answer(1, 1)))
-    WallService.update(Post(3, 1, 1, 2, "Новый год", 5, true, true, true, true, Reposts(10, true, 2), Answer(3, 1)))
+    WallService.clear()
+    WallService.add(Post(1, 1, 1, 1, "Поздравляем Вас с днем рождения", null, true, true, true, true, Reposts(10, true, 1), Answer(1, 1), listOf(PhotoAttachments(Photo(1,1,"https://vk.com/some_photo_link","https://vk.com/another_photo_link")), VideoAttachments(Video(1,1,"A Funny Video",30)))))
+    WallService.add(Post(1, 1, 1, 2, "Новый год", null, true, true, true, true, Reposts(10, true, 2), Answer(1, 1)))
+    WallService.update(Post(3, 1, 1, 2, "Новый год", null, true, true, true, true, Reposts(10, true, 2), Answer(3, 1)))
     WallService.printAllPosts()
 }
-
-
